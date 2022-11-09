930405d7a6436946438a9d4db4a9274d095cb731
Maria Eduarda <eduardaloopes2011@hotmail.com>
2022-11-09 10:39:49 -0300

    data types class finish

--- datatypes.js
+++ datatypes.js
@@ -41,3 +41,45 @@ let children;
 
 // 7- BigInt(ES2020): larger integers than the number type can hold
 // basically it's another type before numbers 
+
+// There is another fundamental thing to know about types which is the fact that javascript has a feature
+// called dynamic typing: this means that when you create a new variable you do not have to manually define
+// the data type of the value that it contains
+// Javascript automatically determines the data type of a value when it's stored into a variable 
+// and this distinction between value and variable is pretty important because in javascript it's the value that has a type, not the variable
+// Value has type, NOT variable!!!
+// variables simply store values that have a type
+
+// Another important thing is that later in our code we can assign a different value for a variable
+// it can start as a number and then become a string
+
+// We can use this to comment our code: 
+/* 
+
+*/
+
+// Typeof its and operator just like the plus or the minus operator that we can use to show he type of a value 
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun); // boolean
+console.log(typeof true); // boolean
+console.log(typeof 23); // number
+console.log(typeof 'Maria'); // string
+
+// We can change the value of a variable just like this:
+javascriptIsFun = 'YES!';
+// and what matters is that we do not write the 'let' again
+// the first time that we declare a new variable we need to do it using let, but then when we want to change the value of the variable,
+// we simply write it again like this but without the let 
+
+// Remember that undefined is the value taken by a variable that is not yet defined, so basically undefined means empty value
+// so every time we declare a variable without a value, it means undefined
+let year;
+// in this case, undefined is the variable AND the value!!!
+
+year = 1999;
+// again, we can reassign the value without the let
+
+
+
+
+
---
+++ letconstvar.js
@@ -0,0 +1,6 @@
+// Section 2
+// 13. Let, const and var
+
+// Let's take a look in the three different ways of declaring variables in JavaScript
+// let and const were introduced in ES6, so they are modern javascript, while the var keyword is the old way of declaring variables
+
