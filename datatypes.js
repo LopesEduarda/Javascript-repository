98f777b49b1d91599a5f338a5ea8825f21e8385a
Maria Eduarda <eduardaloopes2011@hotmail.com>
2022-11-09 09:33:53 -0300

    beggining of the class

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,16 @@
 // Section 2
 // 12. Data Types
 
+// Let's take a look in all the types that we have in JavaScript
+// It's important to remember that in javascript every value is either an object or a primitive value
+
+// This is an object
+let me = { 
+    name: 'Eduarda'
+};
+
+// This is a primitive value
+let firstName = 'Maria';
+let age = 30;
+// A value is only a primitive when it's not an object
+
